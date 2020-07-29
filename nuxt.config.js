285f5e0191e2425d7489d08c70a1e4ff02e2c73e
DaSushiAsian <42751764+DaSushiAsian@users.noreply.github.com>
2020-07-29 12:32:34 +0100

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,14 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+    ],
+    script: [],
   },
   /*
    ** Global CSS
